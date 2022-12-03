8ae5e103c5e90bfae2c04e2743603702576408b1
Brian Voskerijian <brian.voskerijian@7factor.io>
2022-12-03 13:16:48 -0500

    Reduce test duplication

--- event/eventParser.test.ts
+++ event/eventParser.test.ts
@@ -1,27 +1,25 @@
 import { assert, assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { beforeAll, describe, it } from "https://deno.land/std@0.167.0/testing/bdd.ts";
-import { Document, DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+import { CalendarEvent } from "./calendarEvent.ts";
 import EventParser from "./eventParser.ts";
 
-describe('eventParser', () => {
-  let document: Document;
-  let eventParser: EventParser;
+describe('eventParser.parse', () => {
+  let events: CalendarEvent[];
 
   beforeAll(async () => {
     const html = await Deno.readTextFile('test/test.html');
-    document = new DOMParser().parseFromString(html, 'text/html')!;
-    
-    eventParser = new EventParser();
+    const document = new DOMParser().parseFromString(html, 'text/html')!;
+    const eventParser = new EventParser();
+    events = eventParser.parse(document!);
   })
 
   it('parses the correct # of events', () => {
-    const events = eventParser.parse(document!);
     assert(events);
     assertEquals(events.length, 10);
   });
 
   it('parses the event name correctly', () => {
-    const events = eventParser.parse(document!);
     assertEquals(events[0].Name, 'Tree Lighting');
     assertEquals(events[1].Name, 'Board of Trustees Meeting');
     assertEquals(events[2].Name, 'Planning Board Meeting');
