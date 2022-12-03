273de82633e47009b187f179b803a13e77973ebf
Brian Voskerijian <brian.voskerijian@7factor.io>
2022-12-03 17:01:28 -0500

    Add logging

--- main.ts
+++ main.ts
@@ -4,10 +4,22 @@ import EventParser from "./event/eventParser.ts";
 const url = 'https://www.villageofbabylonny.gov/'
 const res = await fetch(url);
 
+console.info(`Fetching content from ${url}`);
+
+const t1 = performance.now();
 const html = await res.text();
 const document = new DOMParser().parseFromString(html, 'text/html')!;
+const t2 = performance.now();
+
+console.info(`Fetched content in ${t2 - t1} milliseconds`);
 
 const eventParser = new EventParser();
 const events = eventParser.parse(document);
 
-await Deno.writeTextFile(Deno.args[0], JSON.stringify(events, null, 2));
+console.info(`Parsed ${events.length} events`);
+
+const outputFilepath = Deno.args[0];
+
+await Deno.writeTextFile(outputFilepath, JSON.stringify(events, null, 2));
+
+console.info(`Wrote events to ${outputFilepath}`);
